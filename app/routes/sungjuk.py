f305bdb1c01ec33f35058348dd24c99e0cb6da4a
Hy30k92 <nakmh92@gmail.com>
2024-07-25 14:17:43 +0900

    /sungjuk 요청에 대한 뷰 추가

--- app/routes/sungjuk.py
+++ app/routes/sungjuk.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from starlette.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
 
 # 라우터 생성
@@ -7,6 +8,13 @@ sungjuk_router = APIRouter()
 templates = Jinja2Templates(directory='views/templates')
 
 # 라우트 설정
+@sungjuk_router.get('/', response_class=HTMLResponse)
+async def sungjuk(req: Request):
+    return templates.TemplateResponse('sungjuk/sungjuk.html',{'request':req})
 
+
+@sungjuk_router.post('/', response_class=HTMLResponse)
+async def sungjukok():
+    pass
 
 
