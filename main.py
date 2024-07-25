2d9b8950d2645fee15e47481530b932392ffec21
Hy30k92 <nakmh92@gmail.com>
2024-07-25 12:19:44 +0900

    탬플릿으로 index.html 응답

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="views/templates")  # jinja2 설정
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+async def index(req: Request):
+    return templates.TemplateResponse('index.html', {'request': req})
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
