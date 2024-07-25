35181cf4ff21537d3fe4266704c4ed4d1076b8d2
Hy30k92 <nakmh92@gmail.com>
2024-07-25 15:20:31 +0900

    /sungjuk 요청에 대해 성적 처리하고 결과를 뷰에 출력

--- app/routes/sungjuk.py
+++ app/routes/sungjuk.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Form
 from starlette.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
 
@@ -14,7 +14,17 @@ async def sungjuk(req: Request):
 
 
 @sungjuk_router.post('/', response_class=HTMLResponse)
-async def sungjukok():
-    pass
-
-
+async def sungjukok(req: Request,
+                    name: str = Form(...) , kor: int = Form(...) ,
+                    eng: int = Form(...) , mat:int = Form(...) ):
+    print(name, kor, eng, mat)
+    tot = kor + eng + mat
+    avg = tot / 3
+    grd =  '가'
+    if (avg >= 90) : grd =  '수'
+    elif (avg >= 80) : grd =  '우'
+    elif (avg >= 70) : grd =  '미'
+    elif (avg >= 60) : grd =  '양'
+    return templates.TemplateResponse('sungjuk/result.html',
+          {'name': name, 'kor': kor, 'eng': eng, 'mat': mat,
+           'tot': tot, 'avg': avg,'grd': grd, 'request': req})
