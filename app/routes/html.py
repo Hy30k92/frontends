5ad4211e389140500034967728a00ad57adbe323
Hy30k92 <nakmh92@gmail.com>
2024-07-26 16:51:50 +0900

    html5 - 텍스트,링크,목록,테이블 관련 태그

--- app/routes/html.py
+++ app/routes/html.py
@@ -8,9 +8,28 @@ templates = Jinja2Templates(directory='views/templates')
 
 # 라우트 설정
 @html_router.get('/')
-async def layout(req: Request):
+async def index(req: Request):
     return templates.TemplateResponse('html/index.html', {'request': req})
 
 @html_router.get('/layout')
 async def layout(req: Request):
     return templates.TemplateResponse('html/01layout.html', {'request': req})
+
+@html_router.get('/text')
+async def text(req: Request):
+    return templates.TemplateResponse('html/02text.html', {'request': req})
+
+@html_router.get('/link')
+async def link(req: Request):
+    return templates.TemplateResponse('html/03link.html', {'request': req})
+
+@html_router.get('/list')
+async def list(req: Request):
+    return templates.TemplateResponse('html/04list.html', {'request': req})
+
+@html_router.get('/table')
+async def table(req: Request):
+    return templates.TemplateResponse('html/05table.html', {'request': req})
+
+
+
