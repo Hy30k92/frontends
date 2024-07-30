ebd57a2a5d8c7c7675fe62c1e1dfe73c5da50475
Hy30k92 <nakmh92@gmail.com>
2024-07-30 18:02:22 +0900

    프로젝트1 실습 - html 작성

--- app/routes/html.py
+++ app/routes/html.py
@@ -52,3 +52,11 @@ async def joinfrm(req: Request):
 @html_router.get('/semantic')
 async def semantic(req: Request):
     return templates.TemplateResponse('html/09semantic.html', {'request': req})
+
+@html_router.get('/project1')
+async def project1(req: Request):
+    return templates.TemplateResponse('html/10project1.html', {'request': req})
+
+@html_router.get('/project2')
+async def project1(req: Request):
+    return templates.TemplateResponse('html/11project2.html', {'request': req})
